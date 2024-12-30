a537eb7b3f15caaf620651d550d89cb2fbd6ce10
Rajeev Singh <rsinghh68@gmail.com>
2024-12-30 20:18:13 +0530

    feat(component/GradientText.tsx): Add GradientText component for text with gradient filled background

---
+++ components/shared/GradientText.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text, TextStyle } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { LinearGradient } from "expo-linear-gradient";
+
+interface GradientTextProps {
+  text: string;
+  style?: TextStyle;
+  colors?: [string, string, ...string[]];
+  locations?: [number, number, ...number[]];
+}
+
+function GradientText({
+  text,
+  style,
+  colors = ["#D556FF", "#FFFFFF", "#D556FF"],
+  locations = [0, 0.52, 1]
+}: GradientTextProps) {
+  return (
+    <MaskedView maskElement={<Text style={style}>{text}</Text>}>
+      <LinearGradient
+        colors={colors}
+        locations={locations}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <Text style={[style, { opacity: 0 }]}>{text}</Text>
+      </LinearGradient>
+    </MaskedView>
+  );
+}
+
+export default GradientText;
