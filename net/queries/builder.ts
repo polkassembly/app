acd72768ab55660f435cd26739a71f9b52307c11
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2025-01-09 22:01:40 +0530

    feat: introduce query builder

--- net/queries/builder.ts
+++ net/queries/builder.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import {
   AxiosInstance,
   AxiosRequestConfig,
@@ -67,6 +67,35 @@ export class MutationBuilder<
           const result = this._responseTransform
             ? this._responseTransform(response)
             : response.data;
+
+          return result;
+        },
+      });
+    };
+  }
+}
+
+export class QueryBuilder<
+  Params = unknown,
+  Result = unknown
+> extends HookBuilder<Params, Result> {
+  build() {
+    return (params: Params) => {
+      return useQuery<Result, Error, Result>({
+        queryKey: [`${this._method} ${this._url}`, params],
+
+        queryFn: async () => {
+          const data = this._requestTransform
+            ? this._requestTransform(params)
+            : params;
+          const response = await this._axios.request({
+            method: this._method,
+            url: this._url,
+            data,
+          });
+          const result = this._responseTransform
+            ? this._responseTransform(response)
+            : (response.data as Result);
 
           return result;
         },
