8f22313a3ab2dfdb9cd3cfa13683b2a27757b7e0
Rajeev Singh <rsinghh68@gmail.com>
2025-01-12 22:55:47 +0530

    fix: Center Badges text in Badges Component

--- components/Profile/Badges.tsx
+++ components/Profile/Badges.tsx
@@ -72,6 +72,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontSize: 18,
