4bf672e556db769e72f2aab15303c365ed454639
Rajeev Singh <rsinghh68@gmail.com>
2025-01-12 22:44:33 +0530

    feat(store): Add setters and getters for type object in store

--- store/index.ts
+++ store/index.ts
@@ -3,6 +3,7 @@ import * as SecureStore from "expo-secure-store";
 // Define constants for keys
 export const KEY_ACCESS_TOKEN = "auth.access";
 export const KEY_REFRESH_TOKEN = "auth.refresh";
+export const PROFILE_DATA = "profile";
 
 // An abstraction for SecureStore
 export const storage = {
@@ -32,4 +33,20 @@ export const storage = {
       console.error(`Error deleting key: ${key}`, error);
     }
   },
+  setObject: (key: string, value: object): void => {
+    try {
+      SecureStore.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error saving key: ${key}`, error);
+    }
+  },
+  getObject: (key: string): object | null => {
+    try {
+      const result = SecureStore.getItem(key);
+      return result ? JSON.parse(result) : null;
+    } catch (error) {
+      console.error(`Error retrieving key: ${key}`, error);
+      return null;
+    }
+  }
 };
