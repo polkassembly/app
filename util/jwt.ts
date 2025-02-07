b0efb7fb383ca9443263251c364f3fa155e5e074
Rajeev Singh <rsinghh68@gmail.com>
2025-02-07 11:52:58 +0530

    fix: add error UI for profile, error handling for id extraction from token

--- util/jwt.ts
+++ util/jwt.ts
@@ -21,7 +21,7 @@ import { jwtDecode } from "jwt-decode";
 //  */
 // const getProfileDataFromToken = (token: string): ProfileData | null => {
 //   try {
-    
+
 //     const decodedToken = jwtDecode<ProfileData & { iat: number; exp: number }>(token);
 
 //     const profileData: ProfileData = {
@@ -50,8 +50,16 @@ import { jwtDecode } from "jwt-decode";
 // };
 
 function getIdFromToken(token: string): string | null {
-  const decodedToken = jwtDecode<{ id: number}>(token);
-  return String(decodedToken.id) || null;
+  try {
+    const decodedToken = jwtDecode<{ id: number }>(token);
+    if (!decodedToken.id) {
+      return null;
+    }
+    return String(decodedToken.id);
+  }catch (error) {
+    console.error('Failed to decode token:', error);
+    return null;
+  }
 }
 
 export default getIdFromToken;
