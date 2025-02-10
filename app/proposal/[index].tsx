c8e1f0ea36f86edfd59d86c75100fb28fb812fd6
Freelancer <freelancerone0519@gmail.com>
2025-02-11 04:04:02 +0530

    feat(proposal-detail): add 'summary' card

--- app/proposal/[index].tsx
+++ app/proposal/[index].tsx
@@ -1,5 +1,6 @@
 import IconBack from "@/components/icons/icon-back";
 import { IconPoints } from "@/components/icons/icon-points";
+import HorizontalSeparator from "@/components/shared/HorizontalSeparator";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { PostCard } from "@/components/timeline/postCard";
@@ -10,6 +11,7 @@ import { Link, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Svg, { G, Rect } from "react-native-svg";
 
 export function TopBar() {
   const textColor = useThemeColor({}, "text");
@@ -74,10 +76,8 @@ export default function ProposalDetailScreen() {
       <View>
         <TopBar />
 
-        <View style={{ paddingInline: 16, paddingBottom: 16 }}>
-          <ThemedText type="titleLarge" style={{ marginBottom: 8 }}>
-            Proposal #{index}
-          </ThemedText>
+        <View style={{ paddingInline: 16, paddingBottom: 16, gap: 8 }}>
+          <ThemedText type="titleLarge">Proposal #{index}</ThemedText>
 
           <StatusBar status={proposal.onChainInfo.status} dot={2500} />
 
@@ -86,6 +86,8 @@ export default function ProposalDetailScreen() {
             withoutViewMore
             containerType="background"
           />
+
+          <Summary status={proposal.onChainInfo.status} />
         </View>
       </View>
     </SafeAreaView>
@@ -110,25 +112,153 @@ function StatusBar({ status, dot }: StatusBarProps) {
         },
       ]}
     >
-      <View
-        style={{
-          paddingBlock: 2,
-          paddingInline: 4,
-          borderRadius: 4,
-          backgroundColor: "#5BC044",
-        }}
-      >
-        <ThemedText
-          type="bodySmall"
-          colorName="background"
-          style={{ textTransform: "uppercase" }}
-        >
-          {status}
-        </ThemedText>
-      </View>
+      <StatusChip status={status} />
 
       <ThemedText type="bodyLarge">{dot}</ThemedText>
     </ThemedView>
+  );
+}
+
+interface SummaryProps {
+  status: string;
+}
+
+function Summary({ status }: SummaryProps) {
+  const backgroundColor = useThemeColor({}, "background");
+
+  return (
+    <ThemedView
+      type="background"
+      style={[
+        styles.box,
+        {
+          alignContent: "stretch",
+          gap: 16,
+        },
+      ]}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <ThemedText type="titleMedium">Summary</ThemedText>
+
+        <StatusChip status={status} />
+      </View>
+
+      <View style={{ gap: 4 }}>
+        <VoteRatioIndicator aye={0.3} />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <ThemedText type="bodySmall" colorName="mediumText">
+            Aye:
+          </ThemedText>
+          <ThemedText type="bodySmall" colorName="mediumText">
+            To pass:
+          </ThemedText>
+          <ThemedText type="bodySmall" colorName="mediumText">
+            Nay:
+          </ThemedText>
+        </View>
+      </View>
+
+      <HorizontalSeparator />
+
+      <View
+        style={{
+          padding: 16,
+          gap: 16,
+        }}
+      >
+        <View
+          style={{
+            gap: 16,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <ThemedText>Aye</ThemedText>
+          <ThemedText colorName="mediumText">23K KSM</ThemedText>
+
+          <ThemedText>Issuance</ThemedText>
+          <ThemedText colorName="mediumText">23K KSM</ThemedText>
+        </View>
+        <View
+          style={{
+            gap: 16,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <ThemedText>Nay</ThemedText>
+          <ThemedText colorName="mediumText">23K KSM</ThemedText>
+
+          <ThemedText>Support</ThemedText>
+          <ThemedText colorName="mediumText">23K KSM</ThemedText>
+        </View>
+      </View>
+    </ThemedView>
+  );
+}
+
+interface StatusChipProps {
+  status: string;
+}
+
+function StatusChip({ status }: StatusChipProps) {
+  return (
+    <View
+      style={{
+        paddingBlock: 2,
+        paddingInline: 4,
+        borderRadius: 4,
+        backgroundColor: "#5BC044",
+      }}
+    >
+      <ThemedText
+        type="bodySmall"
+        colorName="background"
+        style={{ textTransform: "uppercase" }}
+      >
+        {status}
+      </ThemedText>
+    </View>
+  );
+}
+
+interface VoteRatioIndicatorProps {
+  aye: number;
+}
+
+function VoteRatioIndicator({ aye }: VoteRatioIndicatorProps) {
+  const colorAye = "#31C766";
+  const colorNay = "#E5304F";
+
+  const ayePerc = aye * 100;
+  const nayPerc = 100 - ayePerc;
+
+  return (
+    <Svg height={5} viewBox="0 0 100% 100%">
+      <Rect
+        width={`${ayePerc}%`}
+        height={"100%"}
+        rx={"5"}
+        ry={"5"}
+        fill={colorAye}
+      />
+
+      <Rect
+        width={`${nayPerc}%`}
+        x={`${ayePerc}%`}
+        height={"100%"}
+        rx={"2.5"}
+        ry={"2.5"}
+        fill={colorNay}
+      />
+    </Svg>
   );
 }
 
