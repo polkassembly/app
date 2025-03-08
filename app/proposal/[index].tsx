704ab0d2d1afe28a448477c3a4ada52b43fddead
free_dev <freelancerone0519@gmail.com>
2025-03-09 02:04:00 +0530

    Add keyboardAvoidingView for comments

--- app/proposal/[index].tsx
+++ app/proposal/[index].tsx
@@ -5,7 +5,9 @@ import { Link, useLocalSearchParams } from "expo-router";
 import React, { useState } from "react";
 import {
   ActivityIndicator,
+  KeyboardAvoidingView,
   View,
+  Platform,
 } from "react-native";
 import { ProposalDetails } from "@/lib/components/proposal";
 import { PostFullDetails } from "@/lib/components/feed";
@@ -20,51 +22,53 @@ const getTotalLength = (arr: any[]) =>
     return sum + 1 + childrenLength;
   }, 0);
 
-
 export default function ProposalDetailScreenImpl() {
   const [open, setOpen] = useState(false);
-  const { index, proposalType } = useLocalSearchParams<{ index: string, proposalType: EProposalType }>();
-  const proposal = useProposalStore(state => state.proposal);
+  const { index, proposalType } = useLocalSearchParams<{ index: string; proposalType: EProposalType }>();
+  const proposal = useProposalStore((state) => state.proposal);
   const insets = useSafeAreaInsets();
 
-  const backgroundColor = useThemeColor({}, "container");
   const accentColor = useThemeColor({}, "accent");
 
   if (!proposal) {
     return (
-      <View
-        style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
-      >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator color={accentColor} size="large" />
       </View>
     );
   }
 
   return (
-    <View style={{ flex: 1, backgroundColor }}>
-      {/* FIXME: SafeAreaView doesn't works when placed with absolute elements in a view, hence use insets */}
-      <View style={{ flex: 1, paddingHorizontal: 16, marginTop: insets.top }}>
-        <TopBar />
-        <ProposalDetails post={proposal} openFullDetails={() => setOpen(true)} />
-      </View >
+    <View style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={insets.top}
+      >
+        <View style={{ flex: 1, paddingHorizontal: 16 }}>
+          <TopBar />
+          <ProposalDetails post={proposal} openFullDetails={() => setOpen(true)} />
+        </View>
+      </KeyboardAvoidingView>
 
-      {/* Bottom button outside of SafeAreaView */}
-      < View style={{
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        paddingBottom: insets.bottom // Use insets to respect safe area
-      }
-      }>
+      {/* Bottom button outside of KeyboardAvoidingView */}
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          paddingBottom: insets.bottom, // Respect safe area
+        }}
+      >
         <Link asChild href={`/proposal/vote/${index}?proposalType=${proposalType}`}>
           <BottomButton>Cast Your Vote</BottomButton>
         </Link>
-      </View >
+      </View>
 
       <BottomSheet open={open} onClose={() => setOpen(false)}>
         <PostFullDetails onClose={() => setOpen(false)} post={proposal} />
       </BottomSheet>
-    </View >
+    </View>
   );
 }
