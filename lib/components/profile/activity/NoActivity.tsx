af669e8eddbea99ae49c298232b5f70278bcdbbc
free_dev <freelancerone0519@gmail.com>
2025-04-08 13:12:16 +0530

    fix: Fix background color for no activity

--- lib/components/profile/activity/NoActivity.tsx
+++ lib/components/profile/activity/NoActivity.tsx
@@ -1,23 +1,25 @@
 import { Link } from "expo-router";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ThemedText } from "../../shared/text/ThemedText";
-import { ThemedView } from "../../shared/View/ThemedView";
 import { Image } from "react-native";
 
 const NoActivity = () => (
-	<ThemedView style={styles.noActivityContainer}>
-		<Image
-			style={styles.emptyImage}
-			resizeMode="contain"
-			source={require("@/assets/images/profile/empty-activity.png")}
-		/>
-		<ThemedView style={styles.noActivityTextContainer}>
-			<ThemedText>No Activity Yet.</ThemedText>
-			<Link href="/(tabs)">
-				<ThemedText colorName="accent">Vote Now!</ThemedText>
-			</Link>
-		</ThemedView>
-	</ThemedView>
+	<>
+		<ThemedText type="bodySmall">RECENT ACTIVITY</ThemedText>
+		<View style={styles.noActivityContainer}>
+			<Image
+				style={styles.emptyImage}
+				resizeMode="contain"
+				source={require("@/assets/images/profile/empty-activity.png")}
+			/>
+			<View style={styles.noActivityTextContainer}>
+				<ThemedText>No Activity Yet.</ThemedText>
+				<Link href="/(tabs)">
+					<ThemedText colorName="accent">Vote Now!</ThemedText>
+				</Link>
+			</View>
+		</View>
+	</>
 );
 
 const styles = StyleSheet.create({
