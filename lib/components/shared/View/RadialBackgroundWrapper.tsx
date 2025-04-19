dcf2e6e3855bbda4d87ba949ff6ba6cf951f243e
free_dev <freelancerone0519@gmail.com>
2025-04-19 14:05:26 +0530

    chore: Update color of radialBackground to accent

--- lib/components/shared/View/RadialBackgroundWrapper.tsx
+++ lib/components/shared/View/RadialBackgroundWrapper.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 import Svg, { Defs, RadialGradient, Stop, Ellipse } from "react-native-svg";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 import { ThemedView } from "./ThemedView";
+import { useThemeColor } from "@/lib/hooks";
 
 const RadialBackgroundWrapper = ({ children }: PropsWithChildren) => {
   return (
@@ -13,25 +13,26 @@ const RadialBackgroundWrapper = ({ children }: PropsWithChildren) => {
 }
 
 const RadialBackgroundSVG = () => {
+  const accentColor = useThemeColor({}, "accent");
   return (
     <Svg
-        viewBox="0 0 10 10"
-        style={{
-          zIndex: 0,
-          position: "absolute",
-          top: 0,
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <Defs>
-          <RadialGradient id="grad">
-            <Stop offset="0" stopColor={Colors.dark.ctaStroke} stopOpacity={1} />
-            <Stop offset="1" stopColor={Colors.dark.ctaStroke} stopOpacity={0} />
-          </RadialGradient>
-        </Defs>
-        <Ellipse cx="5" cy="40" rx="40" ry="40" fill="url(#grad)" />
-      </Svg>
+      viewBox="0 0 10 10"
+      style={{
+        zIndex: 0,
+        position: "absolute",
+        top: 0,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Defs>
+        <RadialGradient id="grad">
+          <Stop offset="0" stopColor={accentColor} stopOpacity={1} />
+          <Stop offset="1" stopColor={accentColor} stopOpacity={0} />
+        </RadialGradient>
+      </Defs>
+      <Ellipse cx="5" cy="40" rx="40" ry="42" fill="url(#grad)" />
+    </Svg>
   )
 }
 
