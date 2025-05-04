d593112d1fe59cce2612b5a575b760a806851e43
free_dev <freelancerone0519@gmail.com>
2025-05-04 08:54:08 +0530

    fix: Add logged out state in top bar

--- lib/components/shared/Topbar.tsx
+++ lib/components/shared/Topbar.tsx
@@ -1,28 +1,46 @@
-import React, { ReactNode } from "react";
-import { StyleProp, ViewStyle, TouchableOpacity } from "react-native";
-import { Link, router } from "expo-router";
+import React, { memo, ReactNode } from "react";
+import { StyleProp, ViewStyle, TouchableOpacity, StyleSheet } from "react-native";
+import { router } from "expo-router";
 import { View } from "moti";
 import { useThemeColor } from "../../hooks/useThemeColor";
 import IconBack from "../icons/icon-back";
-import { IconPoints } from "../icons/icon-points";
-import { ThemedText } from "./text/ThemedText";
 import { useProfileStore } from "../../store/profileStore";
 import UserAvatar from "./UserAvatar";
+import IconProfile from "../icons/icon-profile";
+import { useAuthModal } from "@/lib/context/authContext";
 
 interface TopBarProps {
   style?: StyleProp<ViewStyle>;
   childrenLink?: string;
   children?: ReactNode;
+  handleBackPress?: () => void;
 }
 
-export default function TopBar({
+const TopBar = ({
   style,
   children,
-}: TopBarProps): JSX.Element {
+  handleBackPress
+}: TopBarProps): JSX.Element => {
+  const [isNavigating, setIsNavigating] = React.useState(false);
+
   const textColor = useThemeColor({}, "text");
   const backgroundColor = useThemeColor({}, "secondaryBackground");
 
-  const userProfile = useProfileStore((state) => state.profile);
+  const user = useProfileStore((state) => state.profile);
+  const address = user?.addresses.length ? user?.addresses[0] : "";
+
+  const { openLoginModal } = useAuthModal();
+
+  const handleProfilePress = () => {
+    if (isNavigating) return;
+    setIsNavigating(true);
+
+    if (user) {
+      router.push(`/profile/${user.id}`);
+    } else {
+      openLoginModal("Login to access your profile", false);
+    }
+  };
 
   return (
     <View
@@ -31,34 +49,64 @@ export default function TopBar({
           backgroundColor,
           flexDirection: "row",
           alignItems: "center",
-          
+
         },
         style,
       ]}
     >
       <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
-        <Link asChild href="..">
-          <TouchableOpacity>
-            <View>
-              <IconBack color={textColor} iconWidth={24} iconHeight={24} />
-            </View>
-          </TouchableOpacity>
-        </Link>
+        <TouchableOpacity
+          onPress={() => {
+            if (handleBackPress) {
+              handleBackPress();
+            } else {
+              router.back();
+            }
+          }}
+        >
+          <View>
+            <IconBack color={textColor} iconWidth={24} iconHeight={24} />
+          </View>
+        </TouchableOpacity>
         {
           children
         }
       </View>
       <View style={{ flexDirection: "row", alignItems: "center", gap: 6 }}>
-        {
-          userProfile?.profileScore && (
-            <TouchableOpacity
-              onPress={() => router.push("/profile")}
-            >
-              <UserAvatar avatarUrl={userProfile?.profileDetails.image} width={24} height={24} />
-            </TouchableOpacity>
-          )
-        }
+        <TouchableOpacity
+          onPress={handleProfilePress}
+          disabled={isNavigating}
+        >
+          {
+            user ? (
+              <UserAvatar
+                address={address}
+                avatarUrl={user?.profileDetails.image || ""}
+                width={24}
+                height={24} />
+            ) : (
+              <View style={[styles.iconContainer, { backgroundColor: backgroundColor }]}>
+                <IconProfile
+                  iconWidth={20}
+                  iconHeight={20}
+                />
+              </View>
+            )
+          }
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
+
+export default memo(TopBar);
